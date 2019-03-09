d9594854b4250ac1a26978be920f25f1776ca849
endieposts <endie@endie.net>
2019-03-09 19:39:57 +0000

    more refactoring; change import.sql property to update

--- src/test/java/com/endie/avizandum/skill/skillapi/SkillApiApplicationTests.java
+++ src/test/java/com/endie/avizandum/skill/skillapi/SkillApiApplicationTests.java
@@ -1,6 +1,6 @@
 package com.endie.avizandum.skill.skillapi;
 
-import com.endie.avizandum.AvizandumApiSkillApplication;
+import com.endie.avizandum.SkillApiApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,10 +8,10 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = AvizandumApiSkillApplication.class)
+@SpringBootTest(classes = SkillApiApplication.class)
 @TestPropertySource(locations= "classpath:test.properties")
 
-public class AvizandumApiSkillApplicationTests {
+public class SkillApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
